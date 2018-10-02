f8789cbe275086eb20b66b0bf4d540e22858260c
rudeboybert <albert.ys.kim@gmail.com>
2018-10-02 18:34:59 -0400

    Posted PS04

---
+++ static/PS/PS04_lastname_firstname.R
@@ -0,0 +1,73 @@
+# Name:
+# I collaborated with:
+
+# The following code loads (i.e. opens) all necessary packages.
+library(ggplot2)
+library(dplyr)
+library(readr)
+
+# Recall the Slack poll in the #polls channel on imposter syndrome. Let's load
+# two representations of the results saved in two CSV's. We do this using the
+# read_csv() function in the readr package.
+poll_results_A <- read_csv("https://rudeboybert.github.io/SDS192/static/PS/poll_results_A.csv")
+View(poll_results_A)
+
+poll_results_B <- read_csv("https://rudeboybert.github.io/SDS192/static/PS/poll_results_B.csv")
+View(poll_results_B)
+
+
+# Question 1 -------------------
+# a) Using the poll_results_A data frame, recreate Fig 1. For all
+# plots don't worry about getting the axes labels and titles to match. Just
+# focus on the bars.
+# Write your ggplot() code below:
+
+
+
+# b) Using the poll_results_A data frame, recreate Fig 2.
+# Write your ggplot() code below:
+
+
+
+# c) Using the poll_results_B data frame, recreate Fig 2.
+# Write your ggplot() code below:
+
+
+
+# d) Using the poll_results_A data frame, recreate Fig 3. Note Fig 3 uses the
+# the default palette.
+# Write your ggplot() code below:
+
+
+
+# e) Using the poll_results_A data frame, recreate Fig 4.
+# Write your ggplot() code below:
+
+
+
+# f) Using the poll_results_B data frame, recreate Fig 4.
+# Write your ggplot() code below:
+
+
+
+# g) Using the poll_results_B data frame, recreate Fig 5.
+# Write your ggplot() code below:
+
+
+
+# h) You find out from Smith administrators that they have been making an effort
+# the last four years to raise awareness about imposter syndrome to normalize it
+# amongst the student body. They want to know how effective they've been over time.
+# IN YOUR OPINION, which of the 5 figures does this job best?
+
+# 
+
+
+# i) Using a calculator (or R!) calculate the proportion of students from each
+# year that have heard of imposter syndrome. An example of a proportion is 0.250
+# indicating 25%
+
+# First-years - The proportion is: 
+# Sophomores - The proportion is:
+# Juniors - The proportion is:
+# Seniors - The proportion is:
